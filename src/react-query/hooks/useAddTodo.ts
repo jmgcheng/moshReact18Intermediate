da536e9e3151b24b17ea5bf5e6d97e667af00e19
jmgcheng <jmgcheng@users.noreply.github.com>
2023-12-17 18:35:22 +0800

    18-fetchingUpdatingDataReactQuery-creatingReusableHttpService

--- src/react-query/hooks/useAddTodo.ts
+++ src/react-query/hooks/useAddTodo.ts
@@ -1,10 +1,8 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { Todo } from "./useTodos";
-import axios from "axios";
 import { CACHE_KEY_TODOS } from "../constants";
-import APIClient from "../services/apiClient";
+import todoService, { Todo } from "../services/todoService";
 
-const apiClient = new APIClient<Todo>('/todos');
+
 
 interface AddTodoContext {
     previousTodos: Todo[];
@@ -14,8 +12,8 @@ const useAddTodo = ( onAdd: () => void ) => {
     const queryClient = useQueryClient();
 
     return useMutation<Todo, Error, Todo, AddTodoContext>({
-        mutationFn: apiClient.post,
-        
+        mutationFn: todoService.post,
+
         onMutate: (newTodo: Todo) => {
             const previousTodos = queryClient.getQueryData<Todo[]>(CACHE_KEY_TODOS) || [];
 
