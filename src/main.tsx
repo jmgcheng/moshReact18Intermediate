0102b807d31130149a9791d7844217e83198dbbc
jmgcheng <jmgcheng@users.noreply.github.com>
2023-12-17 14:44:09 +0800

    08-fetchingUpdatingDataReactQuery-exerciseFetchingData

--- src/main.tsx
+++ src/main.tsx
@@ -6,20 +6,6 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 
-// global setting
-// const queryClient = new QueryClient({
-//   defaultOptions: {
-//     queries: {
-//       retry: 2,
-//       cacheTime: 300_000, //5m
-//       staleTime: 10 * 1000, //10s
-//       refetchOnWindowFocus: false,
-//       refetchOnReconnect: false,
-//       refetchOnMount: false,
-//     },
-//   },
-// });
-
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
