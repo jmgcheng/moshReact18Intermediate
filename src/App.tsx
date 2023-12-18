64a21f42e5ae80b22151d1b3b7a7b52c39ad853f
jmgcheng <jmgcheng@users.noreply.github.com>
2023-12-18 16:11:41 +0800

    33-globalStateManagement-exerciseWorkingWithContext

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,20 @@ import tasksReducers from "./state-management/reducers/tasksReducer";
 import HomePage from "./state-management/HomePage";
 import NavBar from "./state-management/NavBar";
 import TasksContext from "./state-management/contexts/tasksContext";
+import authReducer from "./state-management/reducers/authReducer";
+import AuthContext from "./state-management/contexts/authContext";
 
 function App() {
-  const [tasks, dispatch] = useReducer(tasksReducers, []);
+  const [tasks, tasksDispatch] = useReducer(tasksReducers, []);
+  const [user, authDispatch] = useReducer(authReducer, "");
 
   return (
-    <TasksContext.Provider value={{ tasks, dispatch }}>
-      <NavBar />
-      <HomePage />
-    </TasksContext.Provider>
+    <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+      <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
+        <NavBar />
+        <HomePage />
+      </TasksContext.Provider>
+    </AuthContext.Provider>
   );
 }
 
